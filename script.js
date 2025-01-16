c67ac66b352f98e4f4aad34f2f09f53e88fcd865
alvarobrrss <alvarobarriosalcon@gmail.com>
2025-01-16 09:07:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-// Seleccionar todas las imágenes
 const images = document.querySelectorAll(".slideshow img");
-let currentIndex = 0; // Índice de la imagen actual
+let currentIndex = 0;
 
-// Función para mostrar la siguiente imagen
 function showNextImage() {
-  // Ocultar la imagen actual
   images[currentIndex].classList.remove("active");
-
-  // Calcular el índice de la siguiente imagen
   currentIndex = (currentIndex + 1) % images.length;
-
-  // Mostrar la nueva imagen
   images[currentIndex].classList.add("active");
 }
 
-// Inicializar la primera imagen como visible
 function initSlideshow() {
   if (images.length > 0) {
-    setInterval(showNextImage, 3000); // Cambia de imagen cada 10 segundos
-  } else {
-    console.error("No se encontraron imágenes en el slideshow.");
+    setInterval(showNextImage, 3000);
   }
 }
 
-// Establecer el reloj siempre a 00:00:00
+function generateRandomTime() {
+  const hours = Math.floor(Math.random() * 24).toString().padStart(2, "0");
+  const minutes = Math.floor(Math.random() * 60).toString().padStart(2, "0");
+  const seconds = Math.floor(Math.random() * 60).toString().padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function initBrokenClock() {
+  const clock = document.querySelector('.clock');
+  setInterval(() => {
+    clock.textContent = generateRandomTime();
+  }, 100);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   initSlideshow();
-  const clock = document.querySelector('.clock');
-  clock.textContent = "00:00:00"; // Establecer el reloj a 00:00:00
+  initBrokenClock();
 });
-
