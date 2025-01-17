ba483cebd6094c2e538cbc66767c79b5307e48b0
alvarobrrss <alvarobarriosalcon@gmail.com>
2025-01-17 09:45:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const images = document.querySelectorAll(".slideshow img");
-let currentIndex = 0;
-
-function showNextImage() {
-  images[currentIndex].classList.remove("active");
-  currentIndex = (currentIndex + 1) % images.length;
-  images[currentIndex].classList.add("active");
-}
-
-function initSlideshow() {
-  if (images.length > 0) {
-    setInterval(showNextImage, 3000);
-  }
-}
-
-function generateRandomTime() {
-  const hours = Math.floor(Math.random() * 24).toString().padStart(2, "0");
-  const minutes = Math.floor(Math.random() * 60).toString().padStart(2, "0");
-  const seconds = Math.floor(Math.random() * 60).toString().padStart(2, "0");
-  return `${hours}:${minutes}:${seconds}`;
+function generateRandomDate() {
+  const day = Math.floor(Math.random() * 28 + 1).toString().padStart(2, "0");
+  const month = Math.floor(Math.random() * 12 + 1).toString().padStart(2, "0");
+  const year = Math.floor(Math.random() * (2030 - 1970 + 1) + 1970);
+  return `${day}/${month}/${year}`;
 }
 
 function initBrokenClock() {
   const clock = document.querySelector('.clock');
+  let isPaused = false;
+  let fixedDate = "";
+
+  const intervalId = setInterval(() => {
+    if (!isPaused) {
+      clock.textContent = generateRandomDate();
+    }
+  }, 50);
+
   setInterval(() => {
-    clock.textContent = generateRandomTime();
-  }, 100);
+    isPaused = true;
+    fixedDate = generateRandomDate();
+    clock.textContent = fixedDate;
+
+    setTimeout(() => {
+      isPaused = false;
+    }, 1000);
+  }, 5000);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
