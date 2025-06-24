77ad50de527ea017c7dfc11748055dcf58ce708b
alvarobrrss <alvarobarriosalcon@gmail.com>
2025-06-24 09:02:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-const slideshowContainer = document.querySelector(".slideshow");
 const clock = document.querySelector(".clock");
 const audio = document.getElementById("background-music");
-
-let imagesArray = [];
-let currentIndex = 0;
 let isPaused = false;
-
-for (let i = 1; i <= 26; i++) {
-  let img = document.createElement("img");
-  let imagePath = `bkh_50/imagenbkh (${i}).jpg`;
-  
-  img.src = imagePath;
-  img.alt = `Imagen ${i}`;
-  
-  img.onerror = function() {
-    imagesArray = imagesArray.filter(image => image !== imagePath);
-    if (imagesArray.length > 0) {
-      this.src = imagesArray[Math.floor(Math.random() * imagesArray.length)];
-    }
-  };
-  
-  imagesArray.push(imagePath);
-  if (i === 1) img.classList.add("active");
-  
-  slideshowContainer.appendChild(img);
-}
-
-const images = document.querySelectorAll(".slideshow img");
-
-function showNextImage() {
-  images[currentIndex].classList.remove("active");
-  currentIndex = Math.floor(Math.random() * imagesArray.length);
-  images[currentIndex].classList.add("active");
-}
 
 function generateRandomDate() {
   const day = Math.floor(Math.random() * 28 + 1).toString().padStart(2, "0");
@@ -59,6 +27,5 @@ function animateClock() {
 
 window.addEventListener("DOMContentLoaded", () => {
   animateClock();
-  setInterval(showNextImage, 3000);
   audio.volume = 0.5;
 });
