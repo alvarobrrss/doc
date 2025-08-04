f0fc004e7acaa15b9acca17bd2ecb14b40a51f60
alvarobrrss <alvarobarriosalcon@gmail.com>
2025-08-04 14:59:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-const clock = document.querySelector(".clock");
 const audio = document.getElementById("background-music");
-let isPaused = false;
+audio.volume = 0.5;
 
-function generateRandomDate() {
-  const day = Math.floor(Math.random() * 28 + 1).toString().padStart(2, "0");
-  const month = Math.floor(Math.random() * 12 + 1).toString().padStart(2, "0");
-  const year = Math.floor(Math.random() * (2026 - 1970 + 1) + 1970);
-  return `${day}/${month}/${year}`;
-}
-
-function animateClock() {
-  let speed = 50;
-  let interval = setInterval(() => {
-    if (!isPaused) {
-      clock.textContent = generateRandomDate();
-    }
-  }, speed);
-
-  setInterval(() => {
-    isPaused = true;
-    setTimeout(() => {
-      isPaused = false;
-    }, 1000);
-  }, 3000);
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  animateClock();
-  audio.volume = 0.5;
-});
